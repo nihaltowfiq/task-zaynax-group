5a7baa4805c8fd113835e68d43a28dd73b02d0c9
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-19 18:52:10 +0600

    sliderData added

---
+++ src/data/sliderData.js
@@ -0,0 +1,52 @@
+/* eslint-disable import/no-unresolved */
+import fashion from '../assets/slider/fashion.png';
+import hairCare from '../assets/slider/hairCare.png';
+import mask from '../assets/slider/mask.png';
+import yoga from '../assets/slider/yoga.png';
+
+const sliderData = [
+    {
+        id: 1,
+        img: hairCare,
+        type: 'News',
+        date: 'Oct 12, 2020',
+        comments: 26,
+    },
+    {
+        id: 1,
+        img: mask,
+        type: 'Health',
+        date: 'Oct 12, 2020',
+        comments: 26,
+    },
+    {
+        id: 3,
+        img: fashion,
+        type: 'Fashion',
+        date: 'Oct 12, 2020',
+        comments: 26,
+    },
+    {
+        id: 4,
+        img: yoga,
+        type: 'Health',
+        date: 'Oct 12, 2020',
+        comments: 26,
+    },
+    {
+        id: 5,
+        img: hairCare,
+        type: 'News',
+        date: 'Oct 12, 2020',
+        comments: 26,
+    },
+    {
+        id: 6,
+        img: mask,
+        type: 'Health',
+        date: 'Oct 12, 2020',
+        comments: 26,
+    },
+];
+
+export default sliderData;
