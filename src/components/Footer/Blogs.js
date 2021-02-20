0dbe77eb7279d12e9a507d31b421e71a8336566d
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-20 07:16:32 +0600

    blogs carousel changed

--- src/components/Footer/Blogs.js
+++ src/components/Footer/Blogs.js
@@ -2,24 +2,30 @@
 import { faCalendarAlt, faCommentAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
-import { Card, Col, Container } from 'react-bootstrap';
-import Slider from 'react-slick';
+import { Card, Container } from 'react-bootstrap';
+import Carousel from 'react-elastic-carousel';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import sliderData from '../../data/sliderData';
 import './Blogs.css';
 
-const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true,
-};
+// const settings = {
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     arrows: true,
+// };
 
 function Blogs() {
     const [blogs] = useState(sliderData);
+    const breakPoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 500, itemsToShow: 2 },
+        { width: 768, itemsToShow: 3 },
+        { width: 900, itemsToShow: 4 },
+    ];
     return (
         <Container className="blogs">
             <div className="text-center bg-transparent pt-5 pb-3">
@@ -30,26 +36,24 @@ function Blogs() {
                 </p>
             </div>
 
-            <Slider {...settings}>
+            <Carousel breakPoints={breakPoints}>
                 {blogs.map((blog) => (
-                    <Col key={blog.id}>
-                        <Card className="ml-3 text-white ">
-                            <Card.Img src={blog.img} alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Title>{blog.type}</Card.Title>
-                                <Card.Text className="cardText">
-                                    <span>
-                                        <FontAwesomeIcon icon={faCalendarAlt} /> {blog.date}
-                                    </span>
-                                    <span className="ml-3">
-                                        <FontAwesomeIcon icon={faCommentAlt} /> {blog.comments}
-                                    </span>
-                                </Card.Text>
-                            </Card.ImgOverlay>
-                        </Card>
-                    </Col>
+                    <Card key={blog.id} className="ml-3 text-white ">
+                        <Card.Img src={blog.img} alt="Card image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>{blog.type}</Card.Title>
+                            <Card.Text className="cardText">
+                                <span>
+                                    <FontAwesomeIcon icon={faCalendarAlt} /> {blog.date}
+                                </span>
+                                <span className="ml-3">
+                                    <FontAwesomeIcon icon={faCommentAlt} /> {blog.comments}
+                                </span>
+                            </Card.Text>
+                        </Card.ImgOverlay>
+                    </Card>
                 ))}
-            </Slider>
+            </Carousel>
         </Container>
     );
 }
